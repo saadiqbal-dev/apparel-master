9770f15872a1326ec4397b5cc488bbc4c0604e66
Saad Iqbal <saadiqbal.dev@outlook.com>
2025-12-29 23:25:41 +0500

    fixed herog

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
     <>
       {/* Hero section with video and text */}
       <div className="relative h-[55vh] lg:h-screen overflow-hidden">
-        <VideoBackground videoId="1147812884" fixed={false} />
+        <VideoBackground videoId="1147812884" fixed={true} />
 
         <div className="absolute inset-0 flex items-center justify-center xl:block xl:absolute xl:inset-auto xl:left-0 xl:right-0 xl:top-[39.81vh] xl:w-full z-10">
           <div className="text-center text-white px-4 w-full">
